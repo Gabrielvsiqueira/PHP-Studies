4190dd81037ae515d929e681e6a6ef780d576f55
Gabe Siqueira <gabesiqueira25@gmail.com>
2025-08-19 08:08:47 -0300

    feat: arrays and looops studies

--- laracast/index.php
+++ laracast/index.php
@@ -7,5 +7,21 @@
 </head>
 <body>
   <h1> Recommended Books </h1>
+
+  <?php
+    $books = [
+      'The Pragmatic Programmer',
+      'Clean Code',
+      'Design Patterns: Elements of Reusable Object-Oriented Software',
+      'Refactoring: Improving the Design of Existing Code',
+      'Code Complete'
+    ];
+  ?>
+  
+  <ul>
+    <?php foreach ($books as $book) :?>
+      <li> <?= $book?> </li>
+    <?php endforeach?>
+  </ul>
 </body>
 </html>
